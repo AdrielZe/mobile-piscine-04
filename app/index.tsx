b54894941af02b5c049ed206b67c63e0f07f5ac0
Adriel Silveira De Oliveira <asilveir@student.42sp.org.br>
2025-04-02 22:43:15 -0300

    google auth and login pages

---
+++ app/index.tsx
@@ -0,0 +1,130 @@
+import { ImageBackground, StyleSheet, SafeAreaView, Text, Animated, Easing, View, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useRef, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { useRouter } from 'expo-router';
+
+export default function HomeScreen() {
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+	const checkLoginStatus = async () => {
+		const user = await AsyncStorage.getItem("@user");
+		setIsLoggedIn(!!user);
+		if (user) {
+		    router.replace('./tabs/home-page');
+		}
+		}
+	});
+
+	const handlePress = () => {
+		if (isLoggedIn) {
+		  router.replace('./tabs/home-page');
+		} else {
+		  router.push('./tabs/login-page');
+		}
+	     };
+  const [fontsLoaded] = useFonts({
+    'Jersey15': require('../assets/fonts/Jersey15.ttf'),
+  });
+
+  const floatAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(floatAnim, {
+          toValue: -15,
+          duration: 2000,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: true,
+        }),
+        Animated.timing(floatAnim, {
+          toValue: 0,
+          duration: 2000,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require('../assets/images/lofi-breath.webp')}
+        style={styles.background}
+      >
+        <Animated.View style={[styles.welcomeMsg, { transform: [{ translateY: floatAnim }] }]}>
+          <Text style={styles.welcomeMsgText}>Welcome to your diary app</Text>
+        </Animated.View>
+          <TouchableOpacity 
+            style={styles.buttonContainer} 
+	     onPress={handlePress}
+          >
+            <Text style={styles.loginPageButton}>Log in</Text>
+          </TouchableOpacity>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+// Mantenha seus estilos...
+
+const styles = StyleSheet.create({
+  container: {
+	flex: 1,
+  },
+  background: {
+	flex: 1,
+	width: '100%',
+	height: '100%',
+	justifyContent: 'space-evenly',
+	alignItems: 'center',
+  },
+  welcomeMsg: {
+	width: 300,
+	height: 200,
+	borderRadius: 10,
+	borderWidth: 5,
+	borderColor: '#000',
+	shadowColor: '#000',
+	shadowOffset: { width: 8, height: 18},
+	shadowOpacity: 0.99,
+	shadowRadius: 5,
+	elevation: 10,
+	justifyContent: 'center',
+	alignItems: 'center',
+	backgroundColor: 'rgba(255, 255, 255, 1)',
+  },
+  welcomeMsgText: {
+	fontSize: 40,
+	fontWeight: '100',
+	textAlign: 'center',
+	color: 'black',
+	fontFamily: 'Jersey15',
+  },
+  loginPageButton:{
+	alignItems: 'center',
+	justifyContent: 'center',	
+	textAlign: 'center',
+	fontSize: 20,
+	fontFamily: 'Jersey15',
+  },
+  buttonContainer: {
+	width: 125,
+	height: 50,
+	justifyContent: 'center',
+	backgroundColor: 'rgb(0, 212, 11)',
+	borderRadius: 25,
+	borderWidth: 2,
+  }
+});
---
+++ app/tabs/logged-in-page.tsx
@@ -0,0 +1,117 @@
+import { ImageBackground, StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as React from 'react';
+import { useRouter } from 'expo-router';
+import AuthSession, { resolveDiscoveryAsync } from 'expo-auth-session';
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function LoggedInPage() {
+	const router = useRouter();
+	const [userInfo, setUserInfo] = React.useState(null);
+	const redirectUri =
+		Platform.OS === 'ios'
+		? 'com.googleusercontent.apps.230648280850-8dfin47lp9n9ofss1hojntihr1llmrd2:/oauthredirect'
+		: 'https://auth.expo.io/asilveir/appzao'; 
+	const [request, response, promptAsync] = Google.useAuthRequest({
+		webClientId: '230648280850-8ias4poso58vsct6r1788c60958mos8l.apps.googleusercontent.com',
+		iosClientId: '230648280850-8dfin47lp9n9ofss1hojntihr1llmrd2.apps.googleusercontent.com',
+		redirectUri,
+	}) 
+	const handleLogout = async () => {
+		try {
+			await AsyncStorage.removeItem('@user');
+			setUserInfo(null);
+			router.push('/');
+			console.log('User logged out');
+		} catch (error) {
+			console.error('Error logging out:', error);
+		}
+	}
+	
+	const getUserInfo = async (token : any) => {
+		if (!token)return;
+		try {
+			const response = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			const user = await response.json();
+			await AsyncStorage.setItem("@user", JSON.stringify(user));
+			setUserInfo(user);
+			console.log(token)
+		} catch (error){
+			//error
+		}
+	}
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require('../../assets/images/lofi-breath.webp')}
+        style={styles.background}
+      >
+        <View style={styles.welcomeMsg}>
+	  {<Text style={styles.welcomeMsgText}>{JSON.stringify(userInfo)}</Text>}
+	  {<Text style={styles.welcomeMsgText} onPress={() => {
+				router.push({
+				pathname: '/',
+	     			},)
+				handleLogout();
+			}}
+	   > Logout </Text>}
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  welcomeMsg: {
+    width: 300,
+    height: 200,
+    borderRadius: 10,
+    borderWidth: 5,
+    borderColor: '#000',
+    shadowColor: '#000',
+    shadowOffset: { width: 8, height: 18 },
+    shadowOpacity: 0.99,
+    shadowRadius: 5,
+    elevation: 10,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: 'rgb(255, 255, 255)',
+  },
+  welcomeMsgText: {
+    fontSize: 40,
+    fontWeight: '100',
+    textAlign: 'center',
+    color: 'black',
+    fontFamily: 'Jersey15',
+  },
+  loginPageIcons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+  },
+  iconLoginPage: {
+    borderRadius: 50,
+    borderWidth: 5,
+    padding: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.66)',
+  },
+});
